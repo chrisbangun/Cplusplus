66f4feae51c43a02c94f5a00f1ce56bd7e64b71b
Adi Bangun <adibangun@Adis-MacBook-Pro.local>
2015-06-02 00:15:17 +0100

    Initial Commit

--- Lesson-1/main.cpp
+++ Lesson-1/main.cpp
@@ -7,9 +7,72 @@
 //
 
 #include <iostream>
+using namespace std;
+
+struct node{
+    int data;
+    int count;
+    struct node *left, *right;
+};
+struct node* newNode(int item){
+    struct node *temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = item;
+    temp->count = 1;
+    temp->left = temp->right = NULL;
+    return temp;
+};
+
+void inorder(struct node *root,int &max,int &key){
+    if(root!=NULL){
+        if(root->count > max){
+            max = root->count;
+            key = root->data;
+        }
+        inorder(root->left,max,key);
+      //  cout << root->data << " ";
+        inorder(root->right,max,key);
+        
+    }
+}
+
+struct node* insert(struct node* node, int key){
+    if(node==NULL)
+        return newNode(key);
+    if(key== node->data){
+        (node->count)++;
+        return node;
+    }
+    if(key < node->data){
+        node->left = insert(node->left, key);
+    }else{
+        cout << "here\n";
+        node->right = insert(node->right,key);}
+    
+    return node;
+    
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    struct node *root = NULL;
+    int N;
+    int arr[N+1];
+    cin >> N;
+    int max = 0;
+    int key = 0;
+    for(int i=0;i<N;i++){
+        cin >> arr[i];
+    }
+    int mid = N/2;
+    root = insert(root,arr[mid]);
+    for(int i=0;i<mid;i++)
+        root = insert(root,arr[i]);
+    cout << root->data << "\n";
+    for(int i=mid+1;i<N;i++){
+        cout << arr[i] << "\n";
+        root = insert(root,arr[i]);
+    }
+    inorder(root,max,key);
+   // cout << key << "\n";
     return 0;
 }
---
+++ Lesson-1/max_subsequence_sum/max_subsequence_sum/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  max_subsequence_sum
+//
+//  Created by Adi Bangun on 02/06/2015.
+//  Copyright (c) 2015 Adi Bangun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
